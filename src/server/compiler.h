0c320d8b0e893f396e3e6e6125946bf2e503f5df
michaelskyf <michal.kostrzewski@protonmail.com>
2022-05-31 18:43:35 +0200

    Fix warning on clang build

--- src/server/compiler.h
+++ src/server/compiler.h
@@ -2,12 +2,6 @@
 
 /* Compiler specific quirks */
 
-#ifdef __clang__
-    #define typeof(x) __typeof__(x)
-#else
-    #define typeof(x) __typeof(x)
-#endif
-
 #define __same_type(a, b) __builtin_types_compatible_p(typeof(a), typeof(b))
 
 #define static_assert(expr, ...) __static_assert(expr, ##__VA_ARGS__, #expr)
